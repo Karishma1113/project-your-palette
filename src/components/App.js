30196a48b23e0361b9ba5ab4e93229dcd0295b65
Kevin Tran <iicykdt@gmail.com>
2024-05-17 15:41:39 -0700

    E

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import HomePage from "./Homepage";
 import QuizPage from "./Quizpage";
+import SignInPage from "./components/SignInPage";
 
 
 function App(props) {
   return (
     <div>
       {/* <HomePage /> */}
-      <QuizPage />
+      {/* <QuizPage /> */}
+      {<SignInPage />}
     </div>
   );
 }
