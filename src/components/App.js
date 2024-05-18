0b98b42fe593a8a543a4ecbd7d232de5a402eee0
Karishma Raj <karishmaraj@Karishmas-MacBook-Pro.local>
2024-05-17 18:27:49 -0700

    create pages

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,17 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import HomePage from "./Homepage";
 import QuizPage from "./Quizpage";
 import SignInPage from './SignInPage';
+import Create from './Create';
+import CreateResults from './CreateResults';
 
 function App(props) {
   return (
     <div>
       {/* <HomePage /> */}
-      <QuizPage />
+      {/* <QuizPage /> */}
       {/* {<SignInPage />} */}
+      {/* {<Create/>} */}
+      {<CreateResults/>}
     </div>
   );
 }
