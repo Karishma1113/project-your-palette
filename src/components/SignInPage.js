17b9fd19348f51112748b06ed6b0daa6babbe09c
Fan Bu <fannbuu@gmail.com>
2024-06-02 02:19:53 -0700

    half done the sign in

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -1,51 +1,35 @@
-import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import {getAuth, EmailAuthProvider, GoogleAuthProvider} from "firebase/auth";
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import PageHeader from "./Header";
 import PageFooter from "./Footer";
 
 function SignInPage(props) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
     const navigate = useNavigate();
+    const auth = getAuth();
 
-    const handleInputChange = (event) => {
-        const { name, value } =event.target;
-        if (name === "ID") {
-            setUsername(value);
-        } else if (name === 'password') {
-            setPassword(value)
-        }
-    };
-    
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log('Logging in with', username, password)
-        if (username === "Admin" && password === "123456") {
-            navigate("/profile");
-        } else {
-            alert("Incorrect Username or Password, Username is Admin, Password is 123456")
+    const firebaseUIConfig = {
+        signInOptions: [
+          GoogleAuthProvider.PROVIDER_ID,
+          { provider: EmailAuthProvider.PROVIDER_ID, requiredDisplayName: true },
+        ],
+        signInFlow: 'popup',
+        credentialHelper: 'none',
+        callbacks: {
+            signInSuccessWithAuthResult: () => {
+              return navigate("/profile")
+            }
         }
     }
 
     return (
-        <body>
+        <div>
             <PageHeader />
-            <main className="sign-in">
-                <form onSubmit={handleSubmit}>
-                    <div className="imgcontainer">
-                        <img src="img/palette_logo_without_bg.png" alt="Logo of website Your Palette" />
-                    </div>
-                    <div className="container">
-                        <label htmlFor="ID"><b>Username:</b></label>
-                        <input type="text" placeholder="Enter Username" name="ID" id="ID" required value={username} onChange={handleInputChange} />
-                        <label htmlFor="password"><b>Password:</b></label>
-                        <input type="password" placeholder="Enter Password" name="password" id="password" required value={password} onChange={handleInputChange} />
-                        <button type="submit" className="btn btn-primary btn-sm">Login</button>
-                    </div>
-                </form>
+            <main>
+                <StyledFirebaseAuth firebaseAuth={auth} uiConfig={firebaseUIConfig}/>
             </main>
             <PageFooter />
-        </body>
+        </div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
+import { initializeApp } from "firebase/app";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUsA6MI14sx7yw-RlE3tR9-c6E2PwM9PU",
+  authDomain: "your-pal-ette.firebaseapp.com",
+  databaseURL: "https://your-pal-ette-default-rtdb.firebaseio.com",
+  projectId: "your-pal-ette",
+  storageBucket: "your-pal-ette.appspot.com",
+  messagingSenderId: "87341555622",
+  appId: "1:87341555622:web:716b234337b59e222cb967"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
