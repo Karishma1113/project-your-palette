483fdd53a11bb2a8f8e9502a481593b3347b05a6
lea-sun <leasun@uw.edu>
2024-06-01 19:21:53 -0700

    render create results

--- src/components/CreateResults.js
+++ src/components/CreateResults.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 function CreateResults({ selectedColors }) {
     return (
-        <div>
-            <div className="palette">
-                <div className="palette-colors">
-                    {selectedColors.map((color, index) => (
-                        <div key={index} className="palette-color" style={{ backgroundColor: color }}></div>
-                    ))}
-                </div>
+        <div className="create-results">
+            <h2>Your Color Palette:</h2>
+            <div className="color-palette">
+                {selectedColors.map((color, index) => (
+                    <div
+                        key={index}
+                        className="color-option"
+                        style={{ backgroundColor: color, width: '50px', height: '50px', margin: '5px', border: '1px solid black' }}
+                    ></div>
+                ))}
             </div>
         </div>
     );
