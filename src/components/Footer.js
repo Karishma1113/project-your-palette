ea9f3cf77a3cd0f36152c5534dd2aa498ff2667b
lea-sun <leasun@uw.edu>
2024-06-03 02:33:34 -0700

    fix copyright symbol in footer and adjusted footer in sign in page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 function PageFooter(props) {
     return (
         <footer>
-            <p>&copy Kevin Tran, Leanna Sun, Fan Bu, Karishma Raj, Your Pal-ette 2024</p>
+            <p>&copy; Kevin Tran, Leanna Sun, Fan Bu, Karishma Raj, Your Pal-ette 2024</p>
         </footer>
     )
 }
